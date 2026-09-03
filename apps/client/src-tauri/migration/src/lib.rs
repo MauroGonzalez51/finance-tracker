5a1952d7667cc4f3de74f1709546fbaf4a9b7a3a
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2026-09-03 11:34:28 -0500

    feat: replace app_config migration with device_profile_sessions migration

--- apps/client/src-tauri/migration/src/lib.rs
+++ apps/client/src-tauri/migration/src/lib.rs
@@ -3,7 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20260829_190325_init;
 mod m20260830_152950_create_accounts;
 mod m20260901_211119_payment_methods;
-mod m20260901_222536_app_config;
+mod m20260901_222536_device_profile_sessions;
 mod m20260901_233308_create_categories;
 mod m20260902_130334_create_transactions;
 
@@ -16,7 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20260829_190325_init::Migration),
             Box::new(m20260830_152950_create_accounts::Migration),
             Box::new(m20260901_211119_payment_methods::Migration),
-            Box::new(m20260901_222536_app_config::Migration),
+            Box::new(m20260901_222536_device_profile_sessions::Migration),
             Box::new(m20260901_233308_create_categories::Migration),
             Box::new(m20260902_130334_create_transactions::Migration),
         ]
